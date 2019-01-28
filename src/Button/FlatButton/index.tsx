7237846ab6bca015392f5bdd4d28236e9edfdde9
Lucas <lucas_s.s@hotmail.com>
2019-01-27 22:18:12 -0200

    Adicionado React material UI ao projeto

--- src/Button/FlatButton/index.tsx
+++ src/Button/FlatButton/index.tsx
@@ -1,19 +1,39 @@
 import React, { MouseEventHandler } from 'react';
-import './FlatButton.css';
+import Button from '@material-ui/core/Button';
+// import { withStyles } from '@material-ui/core/styles';
+
+// const styles = (theme: any) => ({
+//   button: {
+//     margin: theme.spacing.unit,
+//   },
+//   input: {
+//     display: 'none',
+//   },
+// });
 
 /**
  * FlatButtonProps properties.
  */
+
 export interface IFlatButtonProps {
   children?: React.ReactNode;
-  disabled: Boolean;
+  disabled: boolean;
   onClick: MouseEventHandler;
+  // classes?: any;
 }
 
-export const FlatButton = (props: IFlatButtonProps) => (
-<div className={props.disabled ? "disabled" : "button"}>
-  <button onClick={props.onClick}>
-    {props.children}
-  </button>
-</div>)
+export const FlatButton = (props: IFlatButtonProps) => {
+  const { children, disabled, onClick } = props;
+  return (
+    <Button
+      onClick={onClick}
+      disabled={disabled}
+      variant="contained"
+      color="secondary"
+    >
+      {children}
+    </Button>
+  )
+}
 
+// export default withStyles(styles)(FlatButton);
